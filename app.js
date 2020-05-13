b1fd33479cd8577732905d9f3343a82e4a0d8140
Monsterbiscuit <46026946+Spupil@users.noreply.github.com>
2020-05-13 19:04:59 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+//app.js
+App({
+  onLaunch() {
+    console.log('App.onLaunch()');
+  },
+  onShow: function () {
+  },
+  onHide: function () {
+  },
+
+  //本应用全局数据
+  globalData: {
+    temperature: {},
+    light: {},
+    humidity: {}
+  }
+})
