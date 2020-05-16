54145e1ad92e2f41a72fa03562f0cdd8004e14d9
ycz-233 <65286160+ycz-233@users.noreply.github.com>
2020-05-16 21:43:47 +0800

    Update subject.js

--- test1/pages/subject/subject.js
+++ test1/pages/subject/subject.js
@@ -1,4 +1,107 @@
 Page({
+  data: {
+    colorArrays: ["#85B8CF", "#90C652", "#D8AA5A", "#FC9F9D", "#0A9A84", "#61BC69", "#12AEF3", "#E29AAD"],
+     wlist: [
+
+     //上课长度全部默认为两节课
+
+      { "xqj": 1, "sksj": 1, "skcd":1, "kcxx": "高等数学 教学楼0218 1~16周"},
+      { "xqj": 1, "sksj": 2, "skcd":1, "kcxx": "高等数学 教学楼0218 1~16周"},
+      { "xqj": 1, "sksj": 3, "skcd": 1, "kcxx": ""},
+      { "xqj": 1, "sksj": 4, "skcd": 1, "kcxx": ""},
+      { "xqj": 1, "sksj": 5, "skcd": 1, "kcxx":"大学英语 教学楼0318 1~16周"},
+      { "xqj": 1, "sksj": 6, "skcd": 1, "kcxx": "" },
+      { "xqj": 1, "sksj": 7, "skcd": 1, "kcxx": "" },
+      { "xqj": 1, "sksj": 8, "skcd": 1, "kcxx": ""},
+      { "xqj": 1, "sksj": 9, "skcd": 1, "kcxx": ""},
+      { "xqj": 1, "sksj": 10, "skcd": 1, "kcxx":"大学英语 教学楼0318 1~16周"},
+      { "xqj": 1, "sksj": 11, "skcd": 1, "kcxx": "" },
+      { "xqj": 1, "sksj": 12, "skcd": 1, "kcxx": "" },
+      { "xqj": 1, "sksj": 13, "skcd": 1, "kcxx": "" },
+      { "xqj": 2, "sksj": 1, "skcd":1, "kcxx": "高等数学 教学楼0218 1~16周"},
+      { "xqj": 2, "sksj": 2, "skcd":1, "kcxx": "高等数学 教学楼0218 1~16周"},
+      { "xqj": 2, "sksj": 3, "skcd": 1, "kcxx": ""},
+      { "xqj": 2, "sksj": 4, "skcd": 1, "kcxx": ""},
+      { "xqj": 2, "sksj": 5, "skcd": 1, "kcxx":"大学英语 教学楼0318 1~16周"},
+      { "xqj": 2, "sksj": 6, "skcd": 1, "kcxx": "" },
+      { "xqj": 2, "sksj": 7, "skcd": 1, "kcxx": "" },
+      { "xqj": 2, "sksj": 8, "skcd": 1, "kcxx": ""},
+      { "xqj": 2, "sksj": 9, "skcd": 1, "kcxx": ""},
+      { "xqj": 2, "sksj": 10, "skcd": 1, "kcxx":"大学英语 教学楼0318 1~16周"},
+      { "xqj": 2, "sksj": 11, "skcd": 1, "kcxx": "" },
+      { "xqj": 2, "sksj": 12, "skcd": 1, "kcxx": "" },
+      { "xqj": 2, "sksj": 13, "skcd": 1, "kcxx": "" },
+      { "xqj": 3, "sksj": 1, "skcd":1, "kcxx": "高等数学 教学楼0218 1~16周"},
+      { "xqj": 3, "sksj": 2, "skcd":1, "kcxx": "高等数学 教学楼0218 1~16周"},
+      { "xqj": 3, "sksj": 3, "skcd": 1, "kcxx": ""},
+      { "xqj": 3, "sksj": 4, "skcd": 1, "kcxx": ""},
+      { "xqj": 3, "sksj": 5, "skcd": 1, "kcxx":"大学英语 教学楼0318 1~16周"},
+      { "xqj": 3, "sksj": 6, "skcd": 1, "kcxx": "" },
+      { "xqj": 3, "sksj": 7, "skcd": 1, "kcxx": "" },
+      { "xqj": 3, "sksj": 8, "skcd": 1, "kcxx": ""},
+      { "xqj": 3, "sksj": 9, "skcd": 1, "kcxx": ""},
+      { "xqj": 3, "sksj": 10, "skcd": 1, "kcxx":"大学英语 教学楼0318 1~16周"},
+      { "xqj": 3, "sksj": 11, "skcd": 1, "kcxx": "" },
+      { "xqj": 3, "sksj": 12, "skcd": 1, "kcxx": "" },
+      { "xqj": 3, "sksj": 13, "skcd": 1, "kcxx": "" },
+      { "xqj": 4, "sksj": 1, "skcd":1, "kcxx": "高等数学 教学楼0218 1~16周"},
+      { "xqj": 4, "sksj": 2, "skcd":1, "kcxx": "高等数学 教学楼0218 1~16周"},
+      { "xqj": 4, "sksj": 3, "skcd": 1, "kcxx": ""},
+      { "xqj": 4, "sksj": 4, "skcd": 1, "kcxx": ""},
+      { "xqj": 4, "sksj": 5, "skcd": 1, "kcxx":"大学英语 教学楼0318 1~16周"},
+      { "xqj": 4, "sksj": 6, "skcd": 1, "kcxx": "" },
+      { "xqj": 4, "sksj": 7, "skcd": 1, "kcxx": "" },
+      { "xqj": 4, "sksj": 8, "skcd": 1, "kcxx": ""},
+      { "xqj": 4, "sksj": 9, "skcd": 1, "kcxx": ""},
+      { "xqj": 4, "sksj": 10, "skcd": 1, "kcxx":"大学英语 教学楼0318 1~16周"},
+      { "xqj": 4, "sksj": 11, "skcd": 1, "kcxx": "" },
+      { "xqj": 4, "sksj": 12, "skcd": 1, "kcxx": "" },
+      { "xqj": 4, "sksj": 13, "skcd": 1, "kcxx": "" },
+      { "xqj": 5, "sksj": 1, "skcd":1, "kcxx": "高等数学 教学楼0218 1~16周"},
+      { "xqj": 5, "sksj": 2, "skcd":1, "kcxx": "高等数学 教学楼0218 1~16周"},
+      { "xqj": 5, "sksj": 3, "skcd": 1, "kcxx": ""},
+      { "xqj": 5, "sksj": 4, "skcd": 1, "kcxx": ""},
+      { "xqj": 5, "sksj": 5, "skcd": 1, "kcxx":"大学英语 教学楼0318 1~16周"},
+      { "xqj": 5, "sksj": 6, "skcd": 1, "kcxx": "" },
+      { "xqj": 5, "sksj": 7, "skcd": 1, "kcxx": "" },
+      { "xqj": 5, "sksj": 8, "skcd": 1, "kcxx": ""},
+      { "xqj": 5, "sksj": 9, "skcd": 1, "kcxx": ""},
+      { "xqj": 5, "sksj": 10, "skcd": 1, "kcxx":"大学英语 教学楼0318 1~16周"},
+      { "xqj": 5, "sksj": 11, "skcd": 1, "kcxx": "" },
+      { "xqj": 5, "sksj": 12, "skcd": 1, "kcxx": "" },
+      { "xqj": 5, "sksj": 13, "skcd": 1, "kcxx": "" },
+      { "xqj": 6, "sksj": 1, "skcd":1, "kcxx": "高等数学 教学楼0218 1~16周"},
+      { "xqj": 6, "sksj": 2, "skcd":1, "kcxx": "高等数学 教学楼0218 1~16周"},
+      { "xqj": 6, "sksj": 3, "skcd": 1, "kcxx": ""},
+      { "xqj": 6, "sksj": 4, "skcd": 1, "kcxx": ""},
+      { "xqj": 6, "sksj": 5, "skcd": 1, "kcxx":"大学英语 教学楼0318 1~16周"},
+      { "xqj": 6, "sksj": 6, "skcd": 1, "kcxx": "" },
+      { "xqj": 6, "sksj": 7, "skcd": 1, "kcxx": "" },
+      { "xqj": 6, "sksj": 8, "skcd": 1, "kcxx": ""},
+      { "xqj": 6, "sksj": 9, "skcd": 1, "kcxx": ""},
+      { "xqj": 6, "sksj": 10, "skcd": 1, "kcxx":"大学英语 教学楼0318 1~16周"},
+      { "xqj": 6, "sksj": 11, "skcd": 1, "kcxx": "" },
+      { "xqj": 6, "sksj": 12, "skcd": 1, "kcxx": "" },
+      { "xqj": 6, "sksj": 13, "skcd": 1, "kcxx": "" },
+      { "xqj": 7, "sksj": 1, "skcd":1, "kcxx": "高等数学 教学楼0218 1~16周"},
+      { "xqj": 7, "sksj": 2, "skcd":1, "kcxx": "高等数学 教学楼0218 1~16周"},
+      { "xqj": 7, "sksj": 3, "skcd": 1, "kcxx": ""},
+      { "xqj": 7, "sksj": 4, "skcd": 1, "kcxx": ""},
+      { "xqj": 7, "sksj": 5, "skcd": 1, "kcxx":"大学英语 教学楼0318 1~16周"},
+      { "xqj": 7, "sksj": 6, "skcd": 1, "kcxx": "" },
+      { "xqj": 7, "sksj": 7, "skcd": 1, "kcxx": "" },
+      { "xqj": 7, "sksj": 8, "skcd": 1, "kcxx": ""},
+      { "xqj": 7, "sksj": 9, "skcd": 1, "kcxx": ""},
+      { "xqj": 7, "sksj": 10, "skcd": 1, "kcxx":"大学英语 教学楼0318 1~16周"},
+      { "xqj": 7, "sksj": 11, "skcd": 1, "kcxx": "" },
+      { "xqj": 7, "sksj": 12, "skcd": 1, "kcxx": "" },
+      { "xqj": 7, "sksj": 13, "skcd": 1, "kcxx": "" },
+
+     
+
+    ]
+
+  },
 
 
 
@@ -8,113 +111,7 @@ Page({
 
    */
 
-  data: {
-
-    colorArrays: ["#85B8CF", "#90C652", "#D8AA5A", "#FC9F9D", "#0A9A84", "#61BC69", "#12AEF3", "#E29AAD"],
-
-    wlist: [
-
-     //上课长度全部默认为两节课
-
-      { "xqj": 1, "sksj": 1, "skcd":2, "kcxx": "高等数学 教学楼0218 1~16周"},
-
-      { "xqj": 1, "sksj": 3, "skcd": 2, "kcxx": ""},
-
-      { "xqj": 1, "sksj": 6, "skcd": 2, "kcxx": ""},
-
-      { "xqj": 1, "sksj": 8, "skcd": 2, "kcxx":"大学英语 教学楼0318 1~16周"},
-
-      { "xqj": 1, "sksj": 10, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 1, "sksj": 12, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 2, "sksj": 1, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 2, "sksj": 3, "skcd": 2, "kcxx": "计算机基础 教学楼0210 1~16周" },
-
-      { "xqj": 2, "sksj": 6, "skcd": 2, "kcxx": "C语言 教学楼0402 1~16周" },
-
-      { "xqj": 2, "sksj": 8, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 2, "sksj": 10, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 2, "sksj": 12, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 3, "sksj": 1, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 3, "sksj": 3, "skcd": 2, "kcxx": "C语言实验课 实验楼0216 4~12周" },
-
-      { "xqj": 3, "sksj": 6, "skcd": 2, "kcxx": "数据结构 教学楼0306 1~16周" },
-
-      { "xqj": 3, "sksj": 8, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 3, "sksj": 10, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 3, "sksj": 12, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 4, "sksj": 1, "skcd": 2, "kcxx": "高等数学 教学楼0218 1~16周" },
-
-      { "xqj": 4, "sksj": 3, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 4, "sksj": 6, "skcd": 2, "kcxx": "乒乓球 体育馆一楼 1~16周" },
-
-      { "xqj": 4, "sksj": 8, "skcd": 2, "kcxx": "数据结构实验课 实验楼0216 4~16周" },
-
-      { "xqj": 4, "sksj": 10, "skcd": 2, "kcxx": "选修课 综实楼0504 1~16周" },
-
-      { "xqj": 4, "sksj": 12, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 5, "sksj": 1, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 5, "sksj": 3, "skcd": 2, "kcxx": "软件工程 教学楼0403 1~16周" },
-
-      { "xqj": 5, "sksj": 6, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 5, "sksj": 8, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 5, "sksj": 10, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 5, "sksj": 12, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 6, "sksj": 1, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 6, "sksj": 3, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 6, "sksj": 6, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 6, "sksj": 8, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 6, "sksj": 10, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 6, "sksj": 12, "skcd": 2, "kcxx": " " },
-
-      { "xqj": 7, "sksj": 1, "skcd": 2, "kcxx": " " },
-
-      { "xqj": 7, "sksj": 3, "skcd": 2, "kcxx": " " },
-
-      { "xqj": 7, "sksj": 6, "skcd": 2, "kcxx": " " },
-
-      { "xqj": 7, "sksj": 8, "skcd": 2, "kcxx": "  " },
-
-      { "xqj": 7, "sksj": 10, "skcd": 2, "kcxx": "" },
-
-      { "xqj": 7, "sksj": 12, "skcd": 2, "kcxx": " " }
-
-    ]
-
-  },
-
-  showCardView: function (e) {
-
-    wx.navigateTo({
-
-      url: '../set/set?id=' + e.currentTarget.id
-
-    });
-
-  },
-
-
+ 
 
   /**
 
