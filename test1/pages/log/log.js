78bf0bc37cc24e12777e8e9ff3c9f5fdacc84863
Monsterbiscuit <46026946+Spupil@users.noreply.github.com>
2020-05-25 11:47:59 +0800

    Update log.js

--- test1/pages/log/log.js
+++ test1/pages/log/log.js
@@ -1,30 +1,49 @@
 // pages/log/log.js
+const LarkCloud = require('./libs/larkcloud-mp-alpha.min.js');
+const serviceId = 'af8581'; // 替换成你的 serviceId，可在后台「设置」页面获取
+// 初始化
+const larkcloud = new LarkCloud({ serviceId });
+
 Page({
-  data: { focus: false,
+  data: { 
 
   },
-  bindButtonTap: function (e) {
-    this.setData({
-      focus: true,
-    })
+  listenerid: function(e) {
+      this.data.id = e.detail.value;
   },
+  listenername: function(e) {
+      this.data.name = e.detail.value;
+  },
+
   onfocus: function (e) {
     console.log(e)
     this.setData({focusDetail: JSON.stringify(e.detail)})
   },
-  onblur: function (e) {
-    tt.showToast({title: 'blur'})
-  },
+
   onconfirm: function (e) {
     tt.showToast({title: 'confirm'})
   },
+  
   onLoad: function (options) {
-
+    var student = tt.getStorageSync('student');
+    console.log('身份认证:',student)
   },
 
-   goToSubject:function(){
-    tt.navigateTo({
+
+   ke:function(){
+    tt.reLaunch({
   url: '/pages/subject/subject', // 指定页面的url
 })
+    console.log('学号: ', this.data.id);
+    console.log('姓名: ', this.data.name);
+    larkcloud.run('identity', {
+num:this.data.id,
+name:this.data.name,
+identity:'student',
+}).then(data => {
+console.log('调用成功',data);
+       }); 
+
   }
+
 })
